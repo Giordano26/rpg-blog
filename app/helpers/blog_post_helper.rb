fd43fa620080117b126507c4ce5059e34256c3f9
Stefano Giordano <smfgiordano@gmail.com>
2025-09-17 00:40:40 -0300

    refact: rubocop fix

--- app/helpers/blog_post_helper.rb
+++ app/helpers/blog_post_helper.rb
@@ -1,10 +1,9 @@
 module BlogPostHelper
   def process_blog_content(content)
     erb_processed = ERB.new(content).result(binding)
-    
+
     renderer = Redcarpet::Render::HTML.new(prettify: true)
     markdown = Redcarpet::Markdown.new(renderer, extensions = { fenced_code_blocks: true })
     markdown.render(erb_processed).html_safe
   end
 end
-
